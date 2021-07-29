d8f50d98496594e39762404f71f121c46d665992
sharun <sharun.msgs@gmail.com>
2021-07-29 19:14:42 +0530

    Auto generate polygon nodes and collision nodes for each district

---
+++ TN.gd
@@ -0,0 +1,80 @@
+extends Node2D
+
+# generated using inkscape --query-all tndistricts.svg
+var d={
+	Ariyalur=[953.4,772.77154],
+	Chengalpattu=[1187.6992,250.59959],
+	Chennai=[1346.5329,127.77688],
+	Coimbatore=[176.59072,778.52605],
+	Cuddalore=[930,608],
+	Dharmapuri=[523.4,391.98666],
+	Dindigul=[378.9,976.16227],
+	Erode=[239.74398,586.9],
+	Kallakurichi=[845.71773,541.92189],
+	Kanchipuram=[1159.6992,209.4353],
+	Kanyakumari=[318.8,1749.4],
+	Karur=[548.7041,882.19391],
+	Krishnagiri=[447.84352,264.0834],
+	Madurai=[448.7,1155.6],
+	Mayiladithurai=[1147.2,766.3],
+	Nagapattinam=[1190.8322,941.2],
+	Namakkal=[525.9,710.24797],
+	Nilgiris=[35.179205,666.66965],
+	Perumbalur=[849.59825,736.1],
+	Pudukotai=[784,1010.1992],
+	Ramanathapuram=[711.41374,1277.6],
+	Ranipet=[1035.4198,153.59961],
+	Salem=[510.87492,581.5356],
+	Sivagangai=[670.3,1117.9],
+	Tenkasi=[342.68731,1464.2479],
+	Thanjavur=[892.9,849.9625],
+	Theni=[349.98068,1189.1748],
+	Thoothukudi=[524.26323,1477.0951],
+	Tiruchirapalli=[687.5,771.2813],
+	Tirunelveli=[342.5811,1598.9395],
+	Tirupathur=[769.5,237.28516],
+	Tiruppur=[295.39381,797.44396],
+	Tiruvallur=[1066.8503,31.7],
+	Tiruvannamalai=[844.8954,260.51825],
+	Tiruvarur=[1067.9132,907.08195],
+	Vellore=[833.79688,171.82608],
+	Vilippuram=[1042.3355,412.88469],
+	Virudhunagar=[406.3,1340.1006]
+}
+
+func create_polygons(s:Sprite):
+	var b=BitMap.new()
+	b.create_from_image_alpha(s.texture.get_data())	
+	var rect=Rect2(0, 0, s.texture.get_width(), s.texture.get_height())
+	var polys=b.opaque_to_polygons(rect)
+	var p=Polygon2D.new()
+	var c=CollisionPolygon2D.new()
+	p.polygon=polys[0]
+	p.position=s.position - Vector2(s.texture.get_width()/2.0,s.texture.get_height()/2.0)
+	p.color=Color.lightcoral
+	c.polygon=polys[0]
+	c.position=s.position - Vector2(s.texture.get_width()/2.0,s.texture.get_height()/2.0)
+	return [c, p]
+
+func _ready():
+	VisualServer.set_default_clear_color(Color(0))
+	for district in d.keys():
+		var a2d=Area2D.new()
+		var x=d[district][0]
+		var y=d[district][1]
+		var s=Sprite.new()
+		var image=load("res://"+district+".png")
+		s.texture=image
+		var w=s.texture.get_width()
+		var h=s.texture.get_height()
+		d[district].append(w)
+		d[district].append(h)
+		var polys=create_polygons(s)
+		a2d.add_child(polys[1])
+		a2d.add_child(polys[0])
+		a2d.name=district
+		a2d.show_behind_parent=true
+		a2d.position=Vector2(x+(w/2),y+(h/2))
+		add_child(a2d)
+		
+		
