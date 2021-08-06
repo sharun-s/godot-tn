2923b413c0491d12a7d8926dac971c038ca442fc
sharun <sharun.msgs@gmail.com>
2021-08-06 10:58:10 +0530

    Disabling touch event handling

--- Camera2D.gd
+++ Camera2D.gd
@@ -15,6 +15,12 @@ func _ready():
 	screen_size = get_viewport_rect().size
 
 func _unhandled_input(event):
+#	if event is InputEventScreenTouch:
+#		if event.pressed:
+#			events[event.index]=event
+#		else:
+#			events.erase(event.index)
+				
 	if event.is_action_pressed("zoom_in"):
 		zoom_level = clamp(zoom_level - zoom_factor, min_zoom, max_zoom)
 		zoom = Vector2(zoom_level, zoom_level)
@@ -31,9 +37,18 @@ func _unhandled_input(event):
 #		# Easing out means we start fast and slow down as we reach the target value.
 #		tween.EASE_OUT)
 #		tween.start()
-	if event is InputEventScreenDrag:
-		events[event.index]=event
-		if events.size() == 1:
-			position+=event.relative.rotated(rotation) * zoom.x
-			position.x=clamp(position.x, 0, screen_size.x)
-			position.y=clamp(position.y, -200, screen_size.y+200)
+#	if event is InputEventScreenDrag:
+#		events[event.index]=event
+#		#if events.size() == 1:
+#		#	position+=event.relative.rotated(rotation) * zoom.x
+#		#	position.x=clamp(position.x, 0, screen_size.x)
+#		#	position.y=clamp(position.y, -200, screen_size.y+200)
+#		if events.size() == 2:
+#			var ekeys=events.keys()
+#			var drag_distance=events[ekeys[1]].position.distance_to(events[ekeys[0]].position)
+#			print(drag_distance)
+#			if abs(drag_distance - last_drag_distance) > 10: #sensitivity
+#				var new_zoom=(1+zoom_factor) if drag_distance < last_drag_distance else (1-zoom_factor)
+#				zoom_level = clamp(new_zoom, min_zoom, max_zoom)
+#				zoom = Vector2(zoom_level, zoom_level)
+#				last_drag_distance=drag_distance
