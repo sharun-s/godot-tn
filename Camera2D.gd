c7c4ea6e8be7324afa869f2ae612371cbb4d870f
sharun <sharun.msgs@gmail.com>
2021-08-20 07:41:21 +0530

    camera cleanup sticking to landscape mode on desktop and mobile for simplicity moved unused compass code to compass.gd added splash

--- Camera2D.gd
+++ Camera2D.gd
@@ -7,49 +7,19 @@ var zoom_factor=0.1
 var zoom_duration=0.2
 var events={}
 var last_drag_distance=0
-onready var tween: Tween = $Tween
-var screen_size
 
 func _ready():
-	screen_size = get_viewport_rect().size
+	pass
 
 func _unhandled_input(event):
-#	if event is InputEventScreenTouch:
-#		if event.pressed:
-#			events[event.index]=event
-#		else:
-#			events.erase(event.index)
-				
+			
 	if event.is_action_pressed("zoom_in"):
 		zoom_level = clamp(zoom_level - zoom_factor, min_zoom, max_zoom)
 		zoom = Vector2(zoom_level, zoom_level)
 		get_parent().get_node("Label").rect_scale=zoom
-#		tween.interpolate_property(self,"zoom",zoom,Vector2(zoom_level, zoom_level),
-#		zoom_level, zoom_duration, tween.TRANS_LINEAR,
-#		# Easing out means we start fast and slow down as we reach the target value.
-#		tween.EASE_OUT)
-		#tween.start()
+
 	if event.is_action_pressed("zoom_out"):
 		zoom_level = clamp(zoom_level + zoom_factor, min_zoom, max_zoom)
 		zoom = Vector2(zoom_level, zoom_level)
 		get_parent().get_node("Label").rect_scale=zoom
-#		tween.interpolate_property(self,"zoom",zoom,Vector2(zoom_level, zoom_level),
-#		zoom_level, zoom_duration, tween.TRANS_SINE,
-#		# Easing out means we start fast and slow down as we reach the target value.
-#		tween.EASE_OUT)
-#		tween.start()
-#	if event is InputEventScreenDrag:
-#		events[event.index]=event
-#		#if events.size() == 1:
-#		#	position+=event.relative.rotated(rotation) * zoom.x
-#		#	position.x=clamp(position.x, 0, screen_size.x)
-#		#	position.y=clamp(position.y, -200, screen_size.y+200)
-#		if events.size() == 2:
-#			var ekeys=events.keys()
-#			var drag_distance=events[ekeys[1]].position.distance_to(events[ekeys[0]].position)
-#			print(drag_distance)
-#			if abs(drag_distance - last_drag_distance) > 10: #sensitivity
-#				var new_zoom=(1+zoom_factor) if drag_distance < last_drag_distance else (1-zoom_factor)
-#				zoom_level = clamp(new_zoom, min_zoom, max_zoom)
-#				zoom = Vector2(zoom_level, zoom_level)
-#				last_drag_distance=drag_distance
+
--- TN.gd
+++ TN.gd
@@ -108,33 +108,6 @@ func merge_poly(history_item):
 	#Color(rng.randf_range(0.1,0.54), rng.randf_range(0.3,0.8), rng.randf_range(0.2,0.91))
 	return main
 	
-var compass_colors={
-	North=[Color.orange,'N'],
-	South=[Color.violet, 'S'],
-	East=['00bb99', 'E'],
-	West=["0099bb", 'W']
-}
-
-func hide_compass():
-	$HUD/E.hide()
-	$HUD/N.hide()
-	$HUD/W.hide()
-	$HUD/S.hide()
-
-func show_compass():
-	$HUD/E.show()
-	$HUD/N.show()
-	$HUD/W.show()
-	$HUD/S.show()
-
-func _compass_toggled(pressed, dir):
-	if game_in_progress==0:
-		if pressed:
-			get_node("HUD/"+compass_colors[dir][1])['modulate']=compass_colors[dir][0]
-			get_tree().set_group(dir, 'modulate', compass_colors[dir][0])
-		else:
-			get_node("HUD/"+compass_colors[dir][1])['modulate']=Color(1.0,1.0,1.0)
-			get_tree().set_group(dir, 'modulate', Color(1.0,1.0,1.0))
 		
 func create_polygons(s:Sprite):
 	var b=BitMap.new()
@@ -194,13 +167,11 @@ func _ready():
 		add_child(a2d)
 		add_label(district, x, y, w, h, "dlabels")
 		a2d.connect('input_event', self, 'on_district_select',[district])
-	$HUD/N.connect("toggled", self, "_compass_toggled", ["North"])
-	$HUD/S.connect("toggled",self, "_compass_toggled", ["South"])
-	$HUD/E.connect("toggled",self, "_compass_toggled", ["East"])
-	$HUD/W.connect("toggled",self, "_compass_toggled", ["West"])
+	
 	$Camera2D.position=get_node('Karur').position
-	$Camera2D/Gopal.position=Vector2(-1*get_viewport().size.x/2+50,0)
-	$Camera2D.zoom=Vector2(2.4, 2.4)
+	$Camera2D/Gopal.position=Vector2(-1*get_viewport().size.x/2-150,0)
+	print(get_viewport_rect().size)#.y/get_viewport_rect().size.x)
+	$Camera2D.zoom=Vector2(2.6, 2.6)
 	$Label.rect_scale=$Camera2D.zoom
 
 
@@ -502,7 +473,7 @@ func game_over():
 	$HUD/Button.show()
 	$HUD/Learn.show()
 	$Camera2D.position=get_node('Karur').position
-	show_compass()
+	#show_compass()
 	game_in_progress=0
 	update() # redraw borders
 	#var tree=get_tree()
@@ -559,7 +530,7 @@ func reset(game_num:=100):
 		$HUD/Learn.hide()
 	else:
 		borders(false)
-	hide_compass()
+	#hide_compass()
 	if selected_district!='':
 		deselect()
 		$Label.text=''
@@ -649,49 +620,6 @@ var district_animator
 func _on_TN_ready():
 	$Timer.start(1)
 	yield($Timer,"timeout")
-	var rd=get_node('Tiruvannamalai')
-	var oa=rd.get_overlapping_areas()
-	for i in oa:
-		if i.name=='Gopal':
-			continue
-		i.add_to_group("North")
-	get_node('Chennai').add_to_group('North')
-	get_node('Tiruvallur').add_to_group('North')
-	get_node('Tiruvannamalai').add_to_group('North')
-	
-	oa=get_node('Virudhunagar').get_overlapping_areas()
-	for i in oa:
-		if i.name=='Gopal':
-			continue
-		i.add_to_group("South")
-	get_node('Virudhunagar').add_to_group('South')
-	oa=get_node('Tirunelveli').get_overlapping_areas()
-	for i in oa:
-		if i.name=='Gopal':
-			continue
-		i.add_to_group("South")
-	get_node('Tirunelveli').add_to_group('South')
-	
-	oa=get_node('Ariyalur').get_overlapping_areas()
-	for i in oa:
-		if i.name=='Gopal':
-			continue
-		i.add_to_group("East")
-	get_node('Ariyalur').add_to_group('East')
-	get_node('Pudukotai').add_to_group('East')
-	get_node('Tiruvarur').add_to_group('East')
-	get_node('Mayiladithurai').add_to_group('East')
-	get_node('Nagapattinam').add_to_group('East')
-		
-	oa=get_node('Tiruppur').get_overlapping_areas()
-	for i in oa:
-		if i.name=='Gopal':
-			continue
-		i.add_to_group("West")
-	get_node('Tiruppur').add_to_group('West')
-	get_node('Namakkal').add_to_group('West')
-	get_node('Salem').add_to_group('West')
-	get_node('Nilgiris').add_to_group('West')
 	#if OS.get_name()=='Android':
 	#	$HUD/Message.anchor_left=.45
 	district_animator=cell.instance()
@@ -714,6 +642,7 @@ func neighbours(districtname):
 		if i.name !='Gopal':
 			n.append(i.name)
 	return n
+
 var years=["1956","1965","1974","1979","1985",
 "1985x2","1986","1989","1991",
 "1993","1995","1996","1997","1997x2",
